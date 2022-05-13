d2ba8db6b0fe0ae01054036c425c329aa38b1b75
DhairyaBhavsar11 <c0816994@mylambton.ca>
2022-05-13 10:13:21 -0600

    change

--- Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/ViewController.swift
@@ -136,7 +136,7 @@ class ViewController: UIViewController
     
     func resetBoard()
     {
-        for button in boardv
+        for button in board
         {
             button.setTitle(nil, for: .normal)
             button.isEnabled = true
